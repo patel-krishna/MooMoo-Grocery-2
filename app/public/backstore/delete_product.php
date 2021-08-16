d6c62bf6fa0f317eddaf95f38bbf9140bbd10ab9
SephoraM <seph156@gmail.com>
2021-08-16 11:47:06 -0400

    added delete functionality

---
+++ app/public/backstore/delete_product.php
@@ -0,0 +1,13 @@
+<?php require_once("../../resources/config.php"); 
+
+if(isset($_GET['id']) && isset($_GET['category'])) {
+    $product_aisle = htmlspecialchars($_GET["category"]);
+    $product_id =  htmlspecialchars($_GET["id"]);
+
+    deleteProductXml($product_aisle, $product_id);
+    header("Location: product-list.php");
+} else {
+    header("Location: product-list.php");
+}
+
+?>
