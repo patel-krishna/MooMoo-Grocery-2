91b3b36390f2a1ffc21f3cf695af2dc82ea33f35
z-bruce <zackrbruce@gmail.com>
2021-08-16 21:53:03 -0400

    added 'add user' button to user list, updated users.xml, added php functionality to user list

--- app/public/backstore/user-list.php
+++ app/public/backstore/user-list.php
@@ -7,7 +7,8 @@
 
 <div class="col-10">
   <h1>User List</h1>
-  <h3>List of MooMoo Grocery user accounts.</h3>
+  <!-- Add user button -->
+  <a class="add-user" href="user-edit.php">Add User</a>
   <table class="backstore-list">
     <tr class="backstore-table-header-row">
       <th class="backstore-table-header">User ID</th>
@@ -18,51 +19,7 @@
       <th class="backstore-table-header">Edit User</th>
       <th class="backstore-table-header">Delete User</th>
     </tr>
-    <tr>
-      <td>0001</td>
-      <td>Bruce</td>
-      <td>Zachary</td>
-      <td class="hide-mobile">zackrbruce@gmail.com</td>
-      <td class="hide-mobile">514 111-1111</td>
-      <td><a class="edit-button" href="user-edit.html">Edit</a></td>
-      <td><a href="#">Delete</a></td>
-    </tr>
-    <tr>
-      <td>0002</td>
-      <td>Kim</td>
-      <td>Youngjae</td>
-      <td class="hide-mobile">youngjae0719@gmail.com</td>
-      <td class="hide-mobile">514 222-2222</td>
-      <td><a class="edit-button" href="user-edit.html">Edit</a></td>
-      <td><a href="#">Delete</a></td>
-    </tr>
-    <tr>
-      <td>0003</td>
-      <td>Maltais</td>
-      <td>Sephora</td>
-      <td class="hide-mobile">seph156@gmail.com</td>
-      <td class="hide-mobile">514 333-3333</td>
-      <td><a class="edit-button" href="user-edit.html">Edit</a></td>
-      <td><a href="#">Delete</a></td>
-    </tr>
-    <tr>
-      <td>0004</td>
-      <td>Lin</td>
-      <td>John</td>
-      <td class="hide-mobile">john.lin@mail.concordia.ca</td>
-      <td class="hide-mobile">514 444-4444</td>
-      <td><a class="edit-button" href="user-edit.html">Edit</a></td>
-      <td><a href="#">Delete</a></td>
-    </tr>
-    <tr>
-      <td>0005</td>
-      <td>Patel</td>
-      <td>Krishna</td>
-      <td class="hide-mobile">krishnaletap@gmail.com</td>
-      <td class="hide-mobile">514 555-5555</td>
-      <td><a class="edit-button" href="user-edit.html">Edit</a></td>
-      <td><a href="#">Delete</a></td>
-    </tr>
+    <?php display_users() ?>
   </table>
 
   <div class="page-numbers-container">
